f9de715e7fa1c733c687802c9509ef6c50736dae
Arthur Andreev <arthur.andreev21@gmail.com>
2020-03-06 22:25:10 +0000

    removed an extra DismissAlert method

--- SimpleSeleniumFramework/Websites/bbc news/Pages/MySportPage.cs
+++ SimpleSeleniumFramework/Websites/bbc news/Pages/MySportPage.cs
@@ -66,7 +66,6 @@ namespace SimpleSeleniumFramework.Websites.bbc_news.Pages
         {
             DismissAlertIfPresent();
             FluentWaitForElementToAppear(By.XPath("//*[@id='my-sport']//span[contains(text(), 'Edit My Sport')]"), 20, 500);
-            DismissAlertIfPresent();
             TakeScreenshot();
             Assert.IsTrue(EditMySport.Enabled);
         }
